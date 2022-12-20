afe90b2bad2c0bb162c1f434ee44ea952b0f85f7
Tamara Dowis <tamara.dowis@gmail.com>
2022-12-20 14:17:16 -0600

    add Medusa, Person, & Statue classes

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,20 @@
-var Person = require('./person');
-var Statue = require('./statue');
+var Person = require("./person");
+var Statue = require("./statue");
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+
+  gazeAtVictim(person) {
+    if (this.statues.length >= 3) {
+      let shiftPerson = this.statues.shift();
+      let newPerson = new Person(shiftPerson.name, "relieved");
+      this.statues.push(new Statue(person.name));
+      return newPerson;
+    } else {
+      this.statues.push(new Statue(person.name));
+    }
+  }
+}
+module.exports = Medusa;
---
+++ mythical-creatures/exercises/person.js
@@ -0,0 +1,7 @@
+class Person {
+    constructor(name, mood) {
+      this.name = name;
+      this.mood = mood || "frightened"
+    }
+}
+module.exports = Person
